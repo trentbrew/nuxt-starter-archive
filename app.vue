616bda01d9a801b79ac721ec794849a0b24628b0
Trent Brew <trent@Trents-MacBook-Air.local>
2022-06-17 12:54:45 -0500

    master

--- app.vue
+++ app.vue
@@ -1,37 +1,11 @@
 <script setup>
-import axios from 'axios';
-
 useHead({
   title: 'Nuxt Starter',
-  htmlAttrs: { 'data-theme': 'black' },
+  htmlAttrs: { 'data-theme': 'emerald' },
 });
-
-function fetchUrl() {
-  axios.get('http://localhost:8000/test').then((res) => {
-    console.log('👉 response from the server: ', res.data);
-  });
-}
 </script>
 
 <template>
-  <main class="w-screen h-screen flex flex-col justify-center items-center gap-[24px]">
-    <img src="assets/nuxt.png" alt="xchange logo" class="h-[50px]" />
-
-    <div class="text-center">
-      <h1 class="text-[36px]">Nuxt 3</h1>
-      <br />
-      <a href="https://v3.nuxtjs.org/guide/directory-structure/components">
-        <strong class="text-[#47b884]">Explore the docs »</strong>
-      </a>
-      <br />
-      <br />
-      <a href="https://tailwindcss.com/docs">TailwindCSS</a>
-      ·
-      <a href="https://daisyui.com/docs">DaisyUI</a>
-      ·
-      <a href="https://app.supabase.com/">Supabase</a>
-    </div>
-
-    <button @click="fetchUrl" class="btn">fetch url</button>
-  </main>
+  <!-- 👇 behaves like router-view -->
+  <NuxtPage />
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<script setup>
+import axios from 'axios';
+
+function fetchUrl() {
+  axios.get('http://localhost:8000/test').then((res) => {
+    console.log('👉 response from the server: ', res.data);
+  });
+}
+</script>
+
+<template>
+  <main class="w-screen h-screen flex flex-col justify-center items-center gap-[24px]">
+    <img src="assets/nuxt.png" alt="xchange logo" class="h-[50px]" />
+
+    <div class="text-center">
+      <h1 class="text-[36px]">Welcome to Nuxt 3</h1>
+      <br />
+      <a href="https://v3.nuxtjs.org/guide/directory-structure/components">
+        <strong class="text-[#47b884]">Explore the docs »</strong>
+      </a>
+      <br />
+      <br />
+      <a href="https://tailwindcss.com/docs">TailwindCSS</a>
+      ·
+      <a href="https://daisyui.com/docs">DaisyUI</a>
+      ·
+      <a href="https://app.supabase.com/">Supabase</a>
+    </div>
+
+    <button @click="fetchUrl" class="btn">fetch url</button>
+    <button @click="fetchUrl" class="btn">fetch url</button>
+  </main>
+</template>
